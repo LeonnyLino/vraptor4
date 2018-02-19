f0c22b0d0f230cd9529b120f2cdabc6a1963f84f
LeonnyLino <leonny.1993@gmail.com>
2018-02-19 00:12:31 -0300

    - Importado bibliotecas do vraptor- jpa para melhor uso. - Usando o simplemail para eviar email automaticos.

--- desenvolvimento-web-com-vraptor-4-master/src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
+++ desenvolvimento-web-com-vraptor-4-master/src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
@@ -2,12 +2,17 @@ package br.com.caelum.vraptor.controller;
 
 import javax.inject.Inject;
 
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.dao.ProdutoDao;
 import br.com.caelum.vraptor.model.Produto;
+import br.com.caelum.vraptor.simplemail.Mailer;
 import br.com.caelum.vraptor.validator.I18nMessage;
 import br.com.caelum.vraptor.validator.Validator;
 import br.com.caelum.vraptor.view.Results;
@@ -18,16 +23,18 @@ public class ProdutoController {
 	private final Result result;
 	private final ProdutoDao dao;
 	private final Validator validator;
+	private final Mailer mailer;
 	
 	@Inject
-	public ProdutoController(Result result, ProdutoDao dao, Validator validator) {
+	public ProdutoController(Result result, ProdutoDao dao, Validator validator, Mailer mailer) {
 		this.result = result;
 		this.dao = dao;
 		this.validator = validator;
+		this.mailer = mailer;
 	}
 	
 	public ProdutoController() {
-		this(null, null, null);
+		this(null, null, null, null);
 	}
 	
 	@Get
@@ -71,6 +78,17 @@ public class ProdutoController {
 	public void remove(Produto produto) {
 		dao.remove(produto);
 		result.include("mensagem", "Protudo Removido com sucesso!");
+		result.redirectTo(this).lista();
+		
+	}
+	
+	@Get
+	public void enviarPedidosDeNovosItens(Produto produto) throws EmailException {
+		Email email = new SimpleEmail();
+		email.setSubject("[vraptor-produto] Precisamos de mais estoque");
+		email.addTo("leonny.1993@gmail.com");
+		email.setMsg("Precisamos de mais itens do produto" + produto.getNome());
+		mailer.send(email);
 		result.redirectTo(this).lista();
 		
 	}
